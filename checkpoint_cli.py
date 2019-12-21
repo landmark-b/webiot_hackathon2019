2f6c38a380d4831a00b1515ee8ad8fb34d78d843
木田祐太郎 <yutaro@lm-Air.local>
2019-12-21 12:54:53 +0900

    Initial commit

---
+++ checkpoint_cli.py
@@ -0,0 +1,52 @@
+import socket
+import json
+import webbrowser
+
+# read config file
+f = open("config_cli.json", "r")
+config = f.read()
+config = json.loads(config) # json to dict
+config_str = json.dumps(config)
+
+M_SIZE = 1024
+
+# Serverのアドレスを用意。Serverのアドレスは確認しておく必要がある。
+serv_address = ('127.0.0.1', 8890)
+
+# ①ソケットを作成する
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    try:
+        # ②messageを送信する
+        print('Input any messages, Type [end] to exit')
+        # message = input()
+        message = config_str
+        if message != 'end':
+            # send_len = sock.sendto(message.encode('utf-8'), serv_address)
+            send_len = sock.sendto(message.encode('utf-8'), serv_address)
+            # ※sendtoメソッドはkeyword arguments(address=serv_addressのような形式)を受け付けないので注意
+
+            # ③Serverからのmessageを受付開始
+            print('Waiting response from Server')
+            rx_meesage, addr = sock.recvfrom(M_SIZE)
+            print(f"[Server]: {rx_meesage.decode(encoding='utf-8')}")
+
+            # TODO: Webにアクセス
+            dict_meesage = json.loads(rx_meesage)
+            url = dict_meesage["url"]
+            webbrowser.open(url)
+
+            break
+
+        else:
+            print('closing socket')
+            sock.close()
+            print('done')
+            break
+
+    except KeyboardInterrupt:
+        print('closing socket')
+        sock.close()
+        print('done')
+        break
