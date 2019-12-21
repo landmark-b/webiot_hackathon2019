371b849d0ae99abcfe1261d1fc7258233c316fd2
Yutaro Kida <kida@sample.com>
2019-12-21 14:19:03 +0900

    checkpoint_sv.py is updated for raspberry pi

--- checkpoint_sv.py
+++ checkpoint_sv.py
@@ -7,7 +7,8 @@ import json
 M_SIZE = 1024
 
 # 
-host = '127.0.0.1'
+# host = '127.0.0.1'
+host = '192.168.11.5'
 port = 8890
 
 locaddr = (host, port)
@@ -18,25 +19,20 @@ config = f.read()
 config = json.loads(config)
 config_str = json.dumps(config)
 
-# ①ソケットを作成する
 sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
 print('create socket')
 
-# ②自ホストで使用するIPアドレスとポート番号を指定
 sock.bind(locaddr)
 
 while True:
     try :
-        # ③Clientからのmessageの受付開始
         print('Waiting message')
         message, cli_addr = sock.recvfrom(M_SIZE)
         message = message.decode(encoding='utf-8')
-        print(f'Received message is [{message}]')
+        print('Received message is [{}]'.format(message))
 
-        # Clientが受信待ちになるまで待つため
         time.sleep(1)
 
-        # ④Clientへ受信完了messageを送信
         print('Send response to Client')
 
         # sock.sendto('Success to receive message'.encode(encoding='utf-8'), cli_addr) 
